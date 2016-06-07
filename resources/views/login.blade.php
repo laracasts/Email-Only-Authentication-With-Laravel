b621b564f1bb0c33a338341e3eb3a2177cb1f895
Nikita Kiselev <kiselev2008@gmail.com>
2016-06-07 11:27:10 +0300

    format list-styled errors output

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -23,9 +23,11 @@
 
         @if ($errors->count())
             <div class="alert alert-danger">
-                @foreach($errors->all() as $error)
-                    {{ $error }}
-                @endforeach
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
             </div>
         @endif
 
